25e44787154ca9bd6a74d4ad915f3dda8076ec25
tri nguyen <ngoctri81297@gmail.com>
2018-05-16 22:33:19 +0700

    update

--- js/home-sendstatus-data.js
+++ js/home-sendstatus-data.js
@@ -1,5 +1,25 @@
 var myStatus = new Firebase('https://glosh-1afd3.firebaseio.com/status');
 var databasestatus = firebase.database();
+var database = firebase.database();
+var userRef = database.ref('users');
+var UserName_global = ""
+//start
+userRef.on('value', takeData);
+function takeData(data){
+  var ss = Object.values( data.val() )
+var email_term = ""
+ firebase.auth().onAuthStateChanged(function(user) {
+  email_term = user.email
+  for (i in ss) {
+      if (ss[i]['Email'] == email_term ){
+        Username = ss[i]['Username']
+       // console.log("====>"+Username)
+        UserName_global = Username
+      }
+    }
+  })
+}
+//end
 $(document).ready(function(){
     $("#statusbtn").click(function(){
     var Datemove = $("#date-move").val();
@@ -12,6 +32,7 @@ $(document).ready(function(){
       From:From,
       To:To,
       Status:Status,
+      UserName: UserName_global
     });
     location.reload("home.html")
     }
--- js/home-showstatus.js
+++ js/home-showstatus.js
@@ -1,10 +1,26 @@
-var query = firebase.database().ref("status").orderByKey();
-query.once("value")
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var key = Snapshot.key;
-      var childData = childSnapshot.val();
-      var childKey = snapshot.child("From").key;
-      console.log(childKey)
-  });
-});
+var database = firebase.database();
+var statusRef = database.ref('status');
+var userRef = database.ref('users');
+userRef.on('value', takeData);
+statusRef.on('value', gotData);
+    function gotData(data){
+    var status = data.val();
+    var keys = Object.keys(status);
+    var str = ""
+    for(var i=0;i<keys.length;i++){
+      var k = keys[i];
+      var username= status[k].UserName;
+      var date = status[k].DepartDay;
+      var from = status[k].From;
+      var to   = status[k].To;
+      var textstatus = status[k].Status;
+      // test
+
+      str += '<tr><th id="Username">Username: '+username+'</th><th id="departday">Depart: '+date+'</th><th id="fromloca">From: '+from+'</th><th id="toloca">To: '+to+'</th></tr>'
+      str += "<tr><td colspan=4><p class='text'>"+textstatus+"</p></td></tr>"
+      str += "<tr><td colspan='4'><button id='inbbtn'>INBOX</button></td></tr><br>"
+      // end test
+      // end ..
+    }
+    $("#info-order").append(str)
+  }
